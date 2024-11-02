d764feb960a60e697c83f467a87f416ace444901
AbedinTelaku <abedintelaku1@gmail.com>
2024-11-02 01:19:51 +0100

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<ContactUs />} />
-      {/* Shto ruter të tjerë këtu */}
     </Routes>
   );
 }
--- src/components/ListOfFilmsComponent/FilmsComponent.jsx
+++ src/components/ListOfFilmsComponent/FilmsComponent.jsx
@@ -1,45 +1,28 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import './FilmStyle.css';
+import React, { useEffect, useState } from 'react';
 
-const FilmsComponent = ({ searchTerm }) => {
+const FilmsComponent = () => {
     const [films, setFilms] = useState([]);
-    const apiKey = import.meta.env.VITE_API_KEY;
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
-            .then(response => response.json())
-            .then(data => setFilms(data.results))
+        fetch('http://localhost:5000/api/popular-movies')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => setFilms(data))
             .catch(error => console.error('Error fetching films:', error));
     }, []);
 
-    const filteredFilms = useMemo(
-        () =>
-            films.filter((film) =>
-                film.title.toLowerCase().includes(searchTerm?.toLowerCase() || '')
-            ),
-        [searchTerm, films]
-    );
-
     return (
-        <div className="films-container">
+        <div>
             <h2>Popular Films</h2>
-            <div className="films-list">
-                {filteredFilms.map((film) => (
-                    <div className="film-card" key={film.id}>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
-                            alt={film.title}
-                        />
-                        <div className="film-info">
-                            <h3>{film.title}</h3>
-                            <p>
-                                {film.release_date} <span className="dot">•</span>{" "}
-                                <span className="genre">Genre</span>
-                            </p>
-                        </div>
-                    </div>
+            <ul>
+                {films.map(film => (
+                    <li key={film.id}>{film.title}</li>
                 ))}
-            </div>
+            </ul>
         </div>
     );
 };
