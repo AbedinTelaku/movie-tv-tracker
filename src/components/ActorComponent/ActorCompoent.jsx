6f699d68d888a912fb991ff54ff3bb3ed6b38085
AbedinTelaku <abedintelaku1@gmail.com>
2024-11-04 17:17:36 +0100

    Create Other Components

---
+++ src/components/ActorComponent/ActorCompoent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActorCompoent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActorCompoent
---
+++ src/components/FaqComponent/FaqPageComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FaqPageComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FaqPageComponent
